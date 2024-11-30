b3a8e3137ffbe6fbc04d32f23bfb2c0f226643a1
hotaco-56 <ng.aiden04@gmail.com>
2024-11-30 00:30:06 -0800

    updated stuff

--- scenes/3d_scenes/player.gd
+++ scenes/3d_scenes/player.gd
@@ -3,24 +3,35 @@ extends CharacterBody3D
 
 const SPEED = 5.0
 const JUMP_VELOCITY = 4.5
+const CAMERA_ROT_SPEED = deg_to_rad(0.9)
 
 #@onready
 #var neck := $neck
 
 var neck
 var camera
+var light
 
 func _ready() -> void:
 	neck = $neck
 	camera = $neck/Camera3D
+	light = $neck/SpotLight3D
 	
 func _unhandled_input(event: InputEvent) -> void:
-	if event is InputEventMouseMotion:
-		neck.rotate_y(-event.relative.x * 0.01)
-		camera.rotate_x(-event.relative.y * 0.01)
+	if event is InputEventMouseButton:
+		Input.mouse_mode = Input.MOUSE_MODE_CAPTURED	
+	
+	if event is InputEventMouseMotion and Input.mouse_mode == Input.MOUSE_MODE_CAPTURED:
+		neck.rotate_y(-event.relative.x * CAMERA_ROT_SPEED)
+		camera.rotate_x(-event.relative.y * CAMERA_ROT_SPEED)
 		camera.rotation.x = clamp(camera.rotation.x, deg_to_rad(-90), deg_to_rad(90))
 
 func _physics_process(delta: float) -> void:
+	
+	#escape mouse
+	if Input.is_action_just_pressed("escape"):
+		Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
+	
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
