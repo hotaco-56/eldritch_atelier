db9e2938eacad4e60f901d7beb7bd69a3c136a31
hotaco-56 <ng.aiden04@gmail.com>
2024-11-29 17:44:12 -0800

    player movement and camera

--- scenes/3d_scenes/player.gd
+++ scenes/3d_scenes/player.gd
@@ -4,6 +4,21 @@ extends CharacterBody3D
 const SPEED = 5.0
 const JUMP_VELOCITY = 4.5
 
+#@onready
+#var neck := $neck
+
+var neck
+var camera
+
+func _ready() -> void:
+	neck = $neck
+	camera = $neck/Camera3D
+	
+func _unhandled_input(event: InputEvent) -> void:
+	if event is InputEventMouseMotion:
+		neck.rotate_y(-event.relative.x * 0.01)
+		camera.rotate_x(-event.relative.y * 0.01)
+		camera.rotation.x = clamp(camera.rotation.x, deg_to_rad(-90), deg_to_rad(90))
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
@@ -16,8 +31,8 @@ func _physics_process(delta: float) -> void:
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var input_dir := Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
-	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	var input_dir := Input.get_vector("left", "right", "forward", "back")
+	var direction = (neck.transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	if direction:
 		velocity.x = direction.x * SPEED
 		velocity.z = direction.z * SPEED
